ef48fc92504d3b1742224b9de70aa13e9037c2e6
jared <jaredgold@proton.me>
2023-09-12 17:33:18 -0400

    write structure as per UML diagram

--- COP2210/COP2210_Lab4/src/vehicle/Car.java
+++ COP2210/COP2210_Lab4/src/vehicle/Car.java
@@ -3,5 +3,71 @@ package vehicle;
 public class Car {
 
   // UML underline mean static
+  static private int vehicleCounter = 2000;
+  static private String CAR_FACTORY_ID = "Norwood";
+
+  private String carID;
+  private String color;
+  private String factory;
+  private int numberOfDoors;
+  private double price;
+  private int mileage;
+  public String doYouLikeIt;
+
+  public Car() {
+
+  }
+
+  public Car(String color, int numberOfDoors) {
+
+  }
+
+  public Car(String color, int numberOfDoors, double price, int mileage) {
+
+  }
+
+  static public int getVehicleCounter() {
+
+  }
+
+  static public void setVehicleCounter(int vehicleCounter) {
+
+  }
+
+  public String getCarID() {
+
+  }
+
+  public String getColor() {
+
+  }
+
+  public void setColor(String color) {
+
+  }
+
+  public String getFactory() {
+
+  }
+
+  public int getNumberOfDoors() {
+
+  }
+
+  public void setNumberOfDoors(int numberOfDoors) {
+
+  }
+
+  public double getPrice() {
+
+  }
+
+  public static void classDisplayInfo() {
+
+  }
+
+  public void displayInfo() {
+
+  }
   
 }
