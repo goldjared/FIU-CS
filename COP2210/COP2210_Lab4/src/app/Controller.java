d33e0ed64bf918e41376e1d513c11a5eac297fbe
jared <jaredgold@proton.me>
2023-09-12 10:54:36 -0400

    create lab 4

---
+++ COP2210/COP2210_Lab4/src/app/Controller.java
@@ -0,0 +1,12 @@
+package app;
+
+public class Controller {
+  public static void main(String[] args) {
+    yourInfoHeader();
+
+  }
+
+  public static void yourInfoHeader() {
+    //insert here
+  }
+}
