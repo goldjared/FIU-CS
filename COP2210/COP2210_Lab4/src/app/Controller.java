4dacc23a688a2636b258ed1c69257a3f28f8a99e
jared <jaredgold@proton.me>
2023-09-17 11:10:20 -0400

    complete assignment

--- COP2210/COP2210_Lab4/src/app/Controller.java
+++ COP2210/COP2210_Lab4/src/app/Controller.java
@@ -11,13 +11,57 @@ public class Controller {
     Car c1 = new Car();
     c1.displayInfo();
 
-    Car c2 = new Car();
+    Car.classDisplayInfo();
+
+    Car c2 = new Car("White", 4);
     c2.displayInfo();
 
-    Car c3 = new Car();
+    Car c3 = new Car("Black", 2);
     c3.displayInfo();
 
-    Car.classDisplayInfo();
+    Car c4 = new Car("Blue", 4, 38500, 10432);
+    c4.displayInfo();
+
+
+    System.out.println();
+    System.out.println("-------------------");
+    System.out.println("working with instance method");
+    System.out.println("-------------------");
+    c4.setNumberOfDoors(2);
+    c4.setColor("Green");
+    c4.displayInfo();
+
+    System.out.println();
+    System.out.println("-------------------");
+    System.out.println("working with public instance variable");
+    System.out.println("-------------------");
+    c2.displayInfo();
+    c2.doYouLikeIt = "No";
+    c2.displayInfo();
+
+    System.out.println();
+    System.out.println();
+    System.out.println("-------------------");
+    System.out.println("working with build-in string method");
+    System.out.println("-------------------");
+    System.out.println();
+    System.out.printf("Color of instance c1: \t\t\t\t\t\t\t%s\n", c1.getColor());
+    System.out.printf("Length of Color of instance c1: \t\t\t%d\n", c1.getColor().length());
+    System.out.println();
+
+    System.out.println("Car ID of instance c2: \t\t\t\t\t\t\t" + c2.getCarID());
+    System.out.printf("Index of 'd' of c2's CarID: \t\t\t\t\t%d\n", c2.getCarID().indexOf("d"));
+    System.out.println();
+
+    System.out.println("Car ID of instance c2: \t\t\t\t\t\t\t" + c2.getCarID());
+    System.out.printf("Index of '0' of c2's CarID: \t\t\t\t%d\n", c2.getCarID().indexOf("o"));
+    System.out.println();
+
+    System.out.println("Car ID of instance c2: \t\t\t\t\t\t\t" + c2.getCarID());
+    System.out.printf("Index of the second 'o' of c2's CarID: \t\t%d", c2.getCarID().indexOf("o", 6));
+    System.out.println();
+
+
   }
 
   public static void yourInfoHeader() {
