f3738af9aa108f795f208519e022486fe13afb3d
jared <jaredgold@proton.me>
2023-09-19 10:45:30 -0400

    write UML code

--- COP2210/COP2210_Lab5/src/cooling/AC.java
+++ COP2210/COP2210_Lab5/src/cooling/AC.java
@@ -1,4 +1,22 @@
 package cooling;
 
 public class AC {
+  private static int numberOfChangedTemperatures = 0;
+  public static void changeTemperatureDown(double currentTemp, double byUnits) {
+
+  }
+
+  public static void changeTemperatureUp(double currentTemp, double byUnits) {
+
+  }
+
+  public static void changeTemperatureDown(Room room, double byUnits) {
+
+  }
+
+  public static void changeTemperatureUp(Room room, double byUnits) {
+
+  }
+
+
 }
