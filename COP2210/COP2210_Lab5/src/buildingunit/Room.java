f21bd71f2b92b9cbd459805a2ebe8d9b4636eb45
jared <jaredgold@proton.me>
2023-09-22 12:17:51 -0400

    write UML code

--- COP2210/COP2210_Lab5/src/buildingunit/Room.java
+++ COP2210/COP2210_Lab5/src/buildingunit/Room.java
@@ -1,4 +1,45 @@
 package buildingunit;
 
 public class Room {
+  private String type;
+  private String color;
+  private double length;
+  private double width;
+  private double temperature;
+
+  public Room(String type, String color, double length, double width, double temperature) {
+
+  }
+
+  public String getType() {return type;}
+  public void setType(String type) {
+
+  }
+
+  public String getColor() {return color;}
+  public void setColor(String color) {
+
+  }
+
+  public double getLength() {return length;}
+
+  public void setLength(double length) {
+
+  }
+
+  public double getWidth() {return width;}
+  public void setWidth(double width) {
+
+  }
+  public double getTemperature(){return temperature;}
+  public void setTemperature(double temperature) {
+
+  }
+  public void roomStats(String tabs) {
+
+  }
+
+  private double calculateSquareFeet() {
+    
+  }
 }
