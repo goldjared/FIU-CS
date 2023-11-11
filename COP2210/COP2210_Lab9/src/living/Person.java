35c311ad292cb01f6cbe2412d232cd4f4aa82878
jared <jaredgold@proton.me>
2023-11-11 12:46:03 -0500

    write person UML

--- COP2210/COP2210_Lab9/src/living/Person.java
+++ COP2210/COP2210_Lab9/src/living/Person.java
@@ -1,4 +1,47 @@
 package living;
 
+import things.BookBag;
+
 public class Person {
+  private String firstName;
+  private String lastName;
+  private BookBag bookBag = new BookBag(null);
+
+  public Person (String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.bookBag.setOwner(this);
+
+  }
+
+ public String getFirstName() {
+    return firstName;
+ }
+
+ public void setFirstName(String firstName) {
+    this.firstName = firstName;
+ }
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName= lastName;
+  }
+
+  public BookBag getBookBag() {
+    return bookBag;
+  }
+
+  public void setBookBag(BookBag bookBag) {
+    this.bookBag = bookBag;
+  }
+
+  public double totalPriceOfPossessions() {
+    //
+  }
+
+
+
+
 }
