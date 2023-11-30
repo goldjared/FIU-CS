e81282bc10976c6cf2a5020f5c9f4ef09f6592c8
jared <jaredgold@proton.me>
2023-11-30 15:44:36 -0500

    write shell code for Fruit

--- COP2210/COP2210_Lab10/src/Fruit.java
+++ COP2210/COP2210_Lab10/src/Fruit.java
@@ -1,2 +1,20 @@
 public class Fruit {
+  private String type;
+  private double weight;
+
+  public Fruit(String type, String weight) {
+    this.type=type;
+    this.weight=  Integer.parseInt(weight);
+  }
+
+
+  public String getType(){return type;}
+  public void setType(String type) {
+    this.type=type;
+  }
+  public double getWeight() { return weight;}
+  public void setWeight(double weight) {this.weight= weight;}
+  public void displayInfo() {
+    //
+  }
 }
