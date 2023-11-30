2b7ee1ee3c62e436f4069762c916677adc5356da
jared <jaredgold@proton.me>
2023-11-30 15:40:04 -0500

    write shell code for Bowl

--- COP2210/COP2210_Lab10/src/Bowl.java
+++ COP2210/COP2210_Lab10/src/Bowl.java
@@ -7,6 +7,22 @@ public class Bowl {
 
   public Bowl(ArrayList<Fruit> fruits, String color, String
               size) {
-    
+    this.fruits=fruits;
+    this.color=color;
+    this.size= Integer.parseInt(size);
+  }
+
+  public ArrayList<Fruit> getFruits() { return fruits;}
+  public void setFruits(ArrayList<Fruit> fruits) {
+    this.fruits=fruits;
+  }
+  public String getColor(){return color;}
+  public void setColor(String color) {
+    this.color=color;
+  }
+  public int getSize() { return size;}
+  public void setSize(int size) {this.size = size;}
+  public void displayInfo() {
+    //
   }
 }
