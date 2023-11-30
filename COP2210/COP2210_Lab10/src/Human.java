ac8ef2a59944d76070dced17c904dac4f888df4a
jared <jaredgold@proton.me>
2023-11-30 16:08:59 -0500

    write shell code for Human

--- COP2210/COP2210_Lab10/src/Human.java
+++ COP2210/COP2210_Lab10/src/Human.java
@@ -1,2 +1,23 @@
 public class Human {
+  private int age;
+  private Car car;
+  public Human(String age) {
+    this.age=Integer.parseInt(age);
+  }
+  public Human(Car car, String age) {
+    this.age=Integer.parseInt(age);
+    this.car=car;
+  }
+
+
+  public Car getCar(){return car;}
+  public void setCar(Car car) {
+  this.car=car;
+  }
+  public int getAge() { return age;}
+  public void setAge(int age) {this.age= age;}
+  public void displayInfo() {
+    //
+  }
+
 }
