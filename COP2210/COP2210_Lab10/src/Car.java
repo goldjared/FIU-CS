3015ed83e58cc2e98fdc8451faf0d815091d0e05
jared <jaredgold@proton.me>
2023-11-30 16:14:02 -0500

    write shell code for Car

--- COP2210/COP2210_Lab10/src/Car.java
+++ COP2210/COP2210_Lab10/src/Car.java
@@ -1,2 +1,24 @@
 public class Car {
+  private double price;
+  private String color;
+  private int numberOfDoors;
+
+  public Car(String price, String color, String numberOfDoors) {
+    this.price= Double.parseDouble(price);
+    this.color=color;
+    this.numberOfDoors= Integer.parseInt(numberOfDoors);
+  }
+  public double getPrice(){return price;}
+  public void setPrice(double price) {
+    this.price=price;
+  }
+  public String getColor(){return color;}
+  public void setColor(String color) {
+    this.color=color;
+  }
+  public int getNumberOfDoors() { return numberOfDoors;}
+  public void setNumberOfDoors(int numberOfDoors) {this.numberOfDoors= numberOfDoors;}
+  public void displayInfo() {
+    //
+  }
 }
