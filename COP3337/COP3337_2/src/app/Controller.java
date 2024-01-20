faa3a4d09ee1ef6dcfe5f607e8b8850ac31e4172
jared <jaredgold@proton.me>
2024-01-20 00:44:42 -0500

    add debug/wip code

--- COP3337/COP3337_2/src/app/Controller.java
+++ COP3337/COP3337_2/src/app/Controller.java
@@ -6,12 +6,17 @@ import java.util.Scanner;
 
 public class Controller {
 
-	public int getAsciiValue(char colLetter) {
+	public static int getAsciiValue(char colLetter) {
 		// return ASCII value of letter, subtract A ASCII val from letter val to convert to 0 based scale of 26 numbers
 		return (int) colLetter - 'A';
 	}
 	
-	public static void main (String[] args) {
+	public static boolean isValid(int row, int col) {
+		// check if the seat selection is valid, throw error if not
+		return row <= 4 && row >= 0 && col >= 0 && col <= 3;
+	}
+	
+	public static void main (String[] args) throws Exception {
 		// create train instance
 		PassengerTrain train1 = new PassengerTrain();
 		// print base seat pattern, empty train
@@ -19,14 +24,47 @@ public class Controller {
 		
 		
 		Scanner sc = new Scanner(System.in);
+	// loop runs so long as the train instance is not full, or 'Q' is pressed in input.
+		while(!train1.isTrainFull()) {
+			boolean validInput = false;
+			int row;
+			int col;
+//			System.out.print("Enter seat row (1-5) and column (A-D) or 'Q' to quit (e.g. '3A'): ");
+//			String scInput = sc.nextLine();
+			String[] scInput;
+			while(!validInput) {
+				System.out.print("Enter seat row (1-5) and column (A-D) or 'Q' to quit (e.g. '3A'): ");
+				scInput = sc.nextLine().split("");
+				if(scInput[0].equals("Q")) {
+					System.out.println("Exiting program.");
+				}
+				
+				row = Integer.parseInt(scInput[0]) - 1;
+				col = getAsciiValue(scInput[1].charAt(0));
+				if(isValid(row, col)) {
+					validInput = true;
+				} else {
+					
+					System.out.println("Invalid seat input.");
+				}
+			}
+//			String[] scInput = sc.nextLine().split("");
+//			if(scInput[0].equals("Q")) {
+//				System.out.println("Exiting program.");
+//			}
+			// if the array length is greater than 2, throw error. invalid.
+			
+			// scanner input, parse and zero base (-1) the row, pass letter value through getAsciiValue method
+			// which also zero bases and returns an integer.
+
+				// the row and col are now integers, and zero based, they are ready to pass into the assignSeat method
+
+				train1.assignSeat(row, col);
+			System.out.println("assigned seat: " + scInput[0] + scInput[1] );
+				
+				train1.displaySeatPattern();
+		}
 		
-		System.out.println("Enter seat row (1-5) and column (A-D) or 'Q' to quit: ");
-		String scInput = sc.nextLine();
-		if(scInput.equals("Q")) {
-			System.out.println("Exiting program.");
-		} else if()
-		String[] seatPos = sc.nextLine().split("");
-		// if the array length is greater than 2, throw error. invalid.
 		
 		
 	}
