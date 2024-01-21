dd4d26ef35d6ba97008ce36cc04568cbfb7cc8e4
jared <jaredgold@proton.me>
2024-01-21 14:21:33 -0500

    change sc input length constraint to equal 2, update comments, reformat

--- COP3337/COP3337_2/src/app/Controller.java
+++ COP3337/COP3337_2/src/app/Controller.java
@@ -1,8 +1,9 @@
 package app;
 
-// import PassengerTrain class from train package
+// Import PassengerTrain class from train package
+
 import train.PassengerTrain;
-// import Scanner from java util
+// Import Scanner from java util
 import java.util.Scanner;
 
 public class Controller {
@@ -25,11 +26,11 @@ public static void main(String[] args) throws Exception {
 	System.out.println("------------------------------------------------");
 	train1.displaySeatPattern();
 	System.out.println("------------------------------------------------");
-	
-	//	Test case:
-	//	Loop fills every seat in the instance except for the last seat(5D).
-	//	Uncomment between the '***' to activate
-	//	***UNCOMMENT LINES BELOW
+
+//		Test case:
+//		Loop fills every seat in the instance except for the last seat(5D).
+//		Uncomment between the '***' to activate
+//		***UNCOMMENT LINES BELOW
 //
 //	for (int i = 0; i < 5; i++) {
 //		for (int j = 0; j < 4; j++) {
@@ -42,52 +43,52 @@ public static void main(String[] args) throws Exception {
 //	train1.displaySeatPattern();
 //	System.out.println("------------------------------------------------");
 //
-	//	***UNCOMMENT LINES ABOVE
+//		***UNCOMMENT LINES ABOVE
 	
 	Scanner sc = new Scanner(System.in);
 	// 3. Loop runs so long as the train instance is not full, or 'Q' is pressed in input.
 	while (!train1.isTrainFull()) {
 		System.out.print("Enter seat row (1-5) and column (A-D) or 'Q' to quit (e.g. '3A'): ");
-		// Put scanner input into an array, splitting between each value in the input.
+		// 4. Put scanner input into an array, splitting between each value in the input.
 		String[] scInput = sc.nextLine().split("");
 		
-		// Check for 'Q' input, exits program via break if so. Break ends the loop.
+		// 5. Check for 'Q' input, exits program via break if so. Break ends the loop.
 		if (scInput[0].equals("Q")) {
 			System.out.println("'Q' Exiting program.");
 			break;
-			// Check input validity via length is greater than 2,
+			// 6. Check input validity via length equals 2,
 			// or via regex checking if the 1st input is not a digit
 			// if so, continue which exits while loop at this point, and loops again.
-		} else if (scInput.length > 2 || !scInput[0].matches("^\\d+$")) {
+		} else if (scInput.length != 2 || !scInput[0].matches("^\\d+$")) {
 			System.out.println("Invalid input.");
 			System.out.println("------------------------------------------------");
 			continue;
 		}
 		
-		// Scanner input, parse and zero base (-1) the row, pass letter value through getAsciiValue method
+		// 7. Clean scanner input by (-1) the row for zero base, pass letter value through getAsciiValue method
 		// which also zero bases and returns an integer.
 		int row = Integer.parseInt(scInput[0]) - 1;
 		int col = getAsciiValue(scInput[1].charAt(0));
-		// Check if scanner input is valid, if not ends loop at this point and restarts
+		// 8. Check if scanner input is valid, if not ends loop at this point and restarts
 		if (!isValid(row, col)) {
 			System.out.println("Invalid seat input.");
 			System.out.println("------------------------------------------------");
 			continue;
-		// Check if the seat available on train instance, if not ends loop at this point and restarts
+			// 9. Check if the seat is available on train instance, if not ends loop at this point and restarts
 		} else if (!train1.isSeatAvailable(row, col)) {
 			System.out.println("Seat not available.");
 			System.out.println("------------------------------------------------");
 			continue;
 		}
 		
-		// The row and col are now ints, sanitized and available. They are ready to pass into the assignSeat method
+		// 10. The row and col are now ints, sanitized and available. Ready to pass into the assignSeat method
 		
 		train1.assignSeat(row, col);
 		System.out.println("SUCCESS Assigned Seat: " + scInput[0] + scInput[1]);
 		System.out.println("------------------------------------------------");
-		// display seat pattern after assigning a seat
+		// 11. Display seat pattern after assigning a seat
 		train1.displaySeatPattern();
 		System.out.println("------------------------------------------------");
-	}
+	} // End while loop
 }
 }
