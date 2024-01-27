27f25e09b31fe64f57ae3a0f41bc24a3cfd430c2
jared <jaredgold@proton.me>
2024-01-26 22:49:56 -0500

    add getMember getItem methods, and refactor class

--- COP3337/COP3337_3/src/lib/Library.java
+++ COP3337/COP3337_3/src/lib/Library.java
@@ -1,23 +1,38 @@
 package lib;
 
+import java.util.ArrayList;
+
 public class Library {
-	LibraryItem[] items;
-	LibraryMember[] members;
+	private final ArrayList<LibraryItem> items;
+	private final ArrayList<LibraryMember> members;
 	
 	public Library() {
-	
+		items = new ArrayList<>();
+		members = new ArrayList<>();
 	}
 	
-	public addItem(LibraryItem item) {
-	
+	public void addItem(LibraryItem item) {
+		items.add(item);
 	}
 	
-	public addMember(LibraryMember member) {
-	
+	public void addMember(LibraryMember member) {
+		members.add(member);
 	}
 	
+	public LibraryMember getMember(int memberId) {
+		for(LibraryMember mem : members) {
+			if(memberId == mem.getMemberId()) return mem;
+		}
+		return null;
+	}
+	public LibraryItem getItem (int itemId) {
+		for(LibraryItem item : items) {
+			if(itemId == item.getItemId()) return item;
+		}
+		return null;
+	}
 	public String toString() {
-	
+		return "Library items: " + items + ", Library members: " + members;
 	}
 	
 }
