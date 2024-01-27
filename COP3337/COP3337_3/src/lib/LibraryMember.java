d49513092b6f97344181fa86a79a6f5397e76181
jared <jaredgold@proton.me>
2024-01-26 22:53:09 -0500

    refactor class

--- COP3337/COP3337_3/src/lib/LibraryMember.java
+++ COP3337/COP3337_3/src/lib/LibraryMember.java
@@ -1,30 +1,38 @@
 package lib;
 
+import java.util.ArrayList;
+
 public class LibraryMember {
 	String name;
 	String address;
 	String contact;
 	int memberId;
-	LibraryItem[] borrowedItems;
+	ArrayList<LibraryItem> borrowedItems;
 	double fines;
 	
 	public LibraryMember(String name, String address, String contact, int memberId) {
+		borrowedItems = new ArrayList<>();
 		this.name = name;
 		this.address = address;
 		this.contact = contact;
 		this.memberId = memberId;
+		fines = 0;
 	}
 	
 	public void borrowItem(LibraryItem item) {
-	
+		borrowedItems.add(item);
 	}
 	
 	public void returnItem(LibraryItem item) {
-	
+		borrowedItems.remove(item);
 	}
 	
-	public String toString() {
+	public int getMemberId() { return memberId; }
 	
+	public String toString() {
+		return "Name: " + name + ", Address: " + address +
+						", Contact: " + contact + ", Member ID: " + memberId +
+						", Borrowed Items: " + borrowedItems + ", Fines: " + fines;
 	}
 	
 
