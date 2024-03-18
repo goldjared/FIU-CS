f4eb436c9a9bba23cced6b0f356ab1122205c902
jared <jaredgold@proton.me>
2024-03-17 20:12:01 -0400

    add documentation, make/delete unique temp file, copy formatted temp file content to new file

--- COP3337/COP3337_7/src/FileIO.java
+++ COP3337/COP3337_7/src/FileIO.java
@@ -1,41 +1,72 @@
 import java.io.*;
 
 public class FileIO {
+	
+	// Main throws IOException for try/catch working with IO
 	public static void main(String[] args) throws IOException {
+		// Declare member values as null initially, to allow scope of member to be accessed/reused
 		File mainFile = null;
 		BufferedReader reader = null;
 		BufferedWriter writer = null;
+		String fileName = "";
 		boolean fileFound = false;
+		// While the file is not found, continue loop
 		while (!fileFound) {
 			System.out.println("Input name of file e.g. 'file.txt'");
 			reader = new BufferedReader(new InputStreamReader(System.in));
-			String fileName = reader.readLine();
+			fileName = reader.readLine();
 			
 			mainFile = new File(fileName);
 			if (!mainFile.exists()) {
 				System.out.println("File name: '" + fileName + "' does not exist");
 				continue;
 			}
+			// Exit loop
 			fileFound = true;
 		}
+		// Starting string for temp file name
+		StringBuilder tempName = new StringBuilder("temp");
+		// Counter to ensure unique suffix for temp file name
+		int counter = 0;
+		// Set File to null, for reuse purposes
+		File tempF = null;
+		// While the tempFile is existing, inc the counter val until the tempFile name does not exist
+		while (fileFound) {
+			// Set tempFile to the unique tempFile name, check if exists, if so, change int value
+			tempF = new File(tempName.toString() + counter + ".txt");
+			if (tempF.exists()) {
+				// Add int value to tempName, to ensure uniqueness
+				counter++;
+				continue;
+			}
+			tempName.append(counter).append(".txt");
+			// Exit loop
+			fileFound = false;
+		}
 		try {
-			writer = new BufferedWriter(new FileWriter("temp.txt"));
+			// Try/catch for setting writer to temp file, and reader to original file
+			writer = new BufferedWriter(new FileWriter(tempName.toString()));
 			String line;
-			reader = new BufferedReader(new FileReader("file.txt"));
+			reader = new BufferedReader(new FileReader(fileName));
+			// Loop runs while the BufferedReader read line is not null
 			while ((line = reader.readLine()) != null) {
 				int lineLen = line.length();
 				String sentence = "";
+				// Add each char value that is not a '.' to the string, once it hits a '.', write string to file
 				for (int i = 0; i < lineLen; i++) {
 					char l = line.charAt(i);
 					if (l != '.') {
 						sentence = sentence + l;
-					} else {
-						System.out.println(sentence);
-						writer.write(sentence.trim());
-						writer.newLine();
-						sentence = "";
+						continue;
 					}
+					// Using trim method to remove whitespace
+					sentence = sentence.trim();
+					// Writing file to current bufferedWriter file (temp file)
+					writer.write(sentence);
+					writer.newLine();
+					sentence = "";
 				}
+				// Close bufferedWriter
 				writer.close();
 			}
 		} catch (IOException e) {
@@ -43,18 +74,27 @@ public class FileIO {
 		}
 		// Copying the contents of temp file to original file
 		try {
-			writer = new BufferedWriter(new FileWriter("file.txt"));
+			// Set bufferedWriter to original file, set reader to the created temp file
+			writer = new BufferedWriter(new FileWriter(fileName));
 			String line;
-			reader = new BufferedReader(new FileReader("temp.txt"));
+			reader = new BufferedReader(new FileReader(tempName.toString()));
 			while ((line = reader.readLine()) != null) {
 				writer.write(line);
 				writer.newLine();
 			}
-			
-			writer.close();
-			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			// Ensure resources are closed and temp file is deleted
+			try {
+				assert writer != null;
+				writer.close();
+				reader.close();
+				tempF.delete();
+				System.out.println("File: '" + fileName + "' edited successfully!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
