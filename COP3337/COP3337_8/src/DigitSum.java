eabcb580050465f21c0f65c2e36a76a3a9344cc7
jared <jaredgold@proton.me>
2024-03-29 16:36:32 -0400

    add documentation as per requirements

--- COP3337/COP3337_8/src/DigitSum.java
+++ COP3337/COP3337_8/src/DigitSum.java
@@ -1,8 +1,12 @@
 public class DigitSum {
 	public static int sumOfDigits(int n) {
+		// base case to end recursion
 		if(n < 10) return n;
+		// modulus to obtain last digit
 		int lastDigit = n % 10;
+		// int (floored value) to obtain the number without the last digit
 		int remainingNum = n / 10;
+		// returning int value with lastDigit + recursive call
 		return lastDigit + sumOfDigits(remainingNum);
 	}
 	
--- COP3337/COP3337_8/src/HandShakes.java
+++ COP3337/COP3337_8/src/HandShakes.java
@@ -1,7 +1,9 @@
 public class HandShakes {
 	public static int totalHandshakes(int n) {
+		// base cases to end recursion
 		if(n <= 1) return 0;
 		if(n == 2) return 1;
+		// returning int value n-1 + recursive call
 		return n-1 + totalHandshakes(n-1);
 	}
 	public static void main(String[] args) {
